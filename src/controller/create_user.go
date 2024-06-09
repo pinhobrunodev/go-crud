1a5c2dd72cc26ae0d853953af21b9d969f62c0ed
pinhobrunodev <iaishowcs@gmail.com>
2024-06-09 10:38:10 -0300

    validation personalization error

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -5,18 +5,17 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pinhobrunodev/go-crud/src/configuration/rest_err"
+	"github.com/pinhobrunodev/go-crud/src/configuration/validation"
 	"github.com/pinhobrunodev/go-crud/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		rest_err := rest_err.NewBadRequestError("Some fields are incorrect")
+		rest_err := validation.ValidateUserError(err)
 		log.Printf("Error: %s", err)
 		c.JSON(rest_err.Code, rest_err)
 		return
-
 	}
 
 	fmt.Println(userRequest)
