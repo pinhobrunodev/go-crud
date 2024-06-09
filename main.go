6e178e3f234310779b69bbc15ec3ab51f1947319
pinhobrunodev <iaishowcs@gmail.com>
2024-06-09 15:31:34 -0300

    logging

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pinhobrunodev/go-crud/src/configuration/logger"
 	"github.com/pinhobrunodev/go-crud/src/controller/routes"
 )
 
 func main() {
+	logger.Info("About to start application...")
 	router := gin.Default()
 
 	routes.InitRoutes(&router.RouterGroup)
--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,22 +1,35 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pinhobrunodev/go-crud/src/configuration/logger"
 	"github.com/pinhobrunodev/go-crud/src/configuration/validation"
 	"github.com/pinhobrunodev/go-crud/src/controller/model/request"
+	"github.com/pinhobrunodev/go-crud/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser Controller", zap.String("journey", "createUser"))
 	var userRequest request.UserRequest
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		rest_err := validation.ValidateUserError(err)
-		log.Printf("Error: %s", err)
+		logger.Error("Error trying to validade user info", err,
+			zap.String("journey", "createUser"))
 		c.JSON(rest_err.Code, rest_err)
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created successfully!", zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 }
